26660889018cee8604280d74744cdb2e822d3035
Thiago Skibinski <thiagoskibinski@gmail.com>
2021-05-10 21:05:55 -0300

    feat: exercises correction

--- 01.module-js/03.commands/06-exercises.js
+++ 01.module-js/03.commands/06-exercises.js
@@ -8,21 +8,32 @@ const value2 = [0, 1, 2, 3, 4];
 const value3 = 12;
 const value4 = "R$ 23,5";
 
+const numberWithoutMoneySymbol = value4.replace('R$ ', '');
+const numberWithPoint = numberWithoutMoneySymbol.replace(',', '.');
+const myValue = parseFloat(numberWithPoint);
+
 // separe a frase por virgula
 const value5 = '0, 2, 3, 4.5, 6, 7';
+value5.split(',');
 
 // encontre a palavra futebol dentro das frases
 const value6 = "Eu gosto de futebol";
 const value7 = "Prefiro no video game";
+value6.includes('futebol');
+value7.includes('futebol');
 
 // transforme a frase em maiscula
 const value8 = "wakanda forever";
+value8.toUpperCase();
 
 // transforme a frase em minuscula
 const value9 = "EU sou O HOMEM FORMIGA";
+value9.toLowerCase();
 
 // altere a palavra futebol por qualquer outra
 const value10 = "eu gosto de Futebol";
+value10.replace('Futebol', 'video game');
 
 // altere as palavras "três" para "dois"
 const value11 = "três tigres tristes comeram três pratos de trigo";
+value11.replaceAll('três', 'dois');
