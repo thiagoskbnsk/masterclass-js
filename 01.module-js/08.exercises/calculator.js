3e1827cbe810b2a7c46b3484e598a8c3ef5e57c7
Thiago <thiagoskibinski@gmail.com>
2021-05-19 20:41:46 -0300

    sistema de finanças

--- 01.module-js/08.exercises/calculator.js
+++ 01.module-js/08.exercises/calculator.js
@@ -1,1 +1,5 @@
-// fazer uma função que retorna um objeto com todas as funções básicas de uma calculadora (+, -, /, *);
+// com base em um numero de entrada, faça:
+// uma função que retorna um objeto com todas as funções básicas de uma calculadora (+, -, /, *);
+// uma função que retorne um historico de tudo que foi executado
+// uma função que reseta tudo, exceto historico. No historico deve conter que foi resetado
+// uma funcao que mostra o historico.
--- 01.module-js/08.exercises/finances.js
+++ 01.module-js/08.exercises/finances.js
@@ -1,7 +1,46 @@
+// salario
 // entradas
-
 // saídas
+// balanço
 
-// somatória
+// se o que sobrar da renda total for menor que 10% imprima "Precisamos economizar", 
+// se for mais que 10% e menor que 30% "estamos indo bem", 
+// se for maior que 30% "Vamos começar a investir!"
 
-// se o que sobrar da renda total for menor que 10% imprima "Precisamos economizar", se for mais que 10% e menor que 30% "estamos indo bem", se for maior que 30% "Vamos começar a investir!"
+const financas = (salario) => {
+  let balanco = salario;
+
+  const entrada = (valor) => {
+    balanco += valor;
+  }
+
+  const saida = (valor) => {
+    balanco -= valor;
+  }
+
+  const total = () => {
+    const porcentagem = (balanco / salario) * 100;
+    let textoFinal = 'Vamos começar a investir!';
+
+    if (porcentagem < 10) {
+      textoFinal = 'Precisamos economizar!';
+    } else if (porcentagem >= 10 && porcentagem < 30) {
+      textoFinal = 'Estamos indo bem!';
+    }
+
+    console.log(`${textoFinal} Porcentagem: ${porcentagem}%`)
+  }
+
+  return {
+    entrada,
+    saida,
+    total,
+  }
+}
+
+const sistema = financas(1000);
+
+sistema.total();
+sistema.saida(900);
+sistema.entrada(400);
+sistema.total();
