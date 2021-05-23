17194f084afe305f9e1b51af6f1fafa6c3e9a8ef
Thiago <thiagoskibinski@gmail.com>
2021-05-23 11:16:48 -0300

    feat: exercises solved

--- 01.module-js/06.loops/06-exercises.js
+++ 01.module-js/06.loops/06-exercises.js
@@ -1,16 +1,49 @@
 // imprima o valor do array ["1", "2", "3"]
+const array = ["1", "2", "3"];
+array.forEach(value => {
+  console.log(value);
+});
 
 // converta o array em numero e imprima o valor ["1", "2", "3"]
+const array = ["1", "2", "3"];
+array.forEach(value => {
+  console.log(Number(value));
+});
 
 // remova todos os números 2 do array [0, 3, 2, 1, 2, 4, 6, 2];
+const array = [0, 3, 2, 1, 2, 4, 6, 2];
+const novoArray = array.filter(value => value !== 2);
+console.log(novoArray);
 
 // dado o array, retorne um objeto contendo { valor: número }
 const value1 = [1, 2, 3, 4, 5];
+const objeto = value1.reduce((acc, value) => {
+  return {
+    ...acc,
+    valor: value,
+  }
+}, {});
+console.log(objeto);
 
 // faça a soma da chave "numero" dentro do objeto
 const value2 = [{ id: 0, numero: 4 }, { id: 1, numero: 9 }, { id: 2, numero: "2" }, { id: 3, numero: 8 }]
+const soma = value2.reduce((acc, value) => {
+  return acc += value.numero;
+}, 0);
+console.log(soma);
 
 // percorra o array, se encontrar o número 50, pare. Imprima todos os números até parar usando while
 const value3 = [...new Array(100)];
+let i = 0;
+while(i < value3.length) {
+  console.log(i);
+
+  if (i === 50) break;
+}
 
 // imprima apenas os numeros pares de 1 a 50 usando (for)
+for (let i = 0; i <= 50; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
--- 01.module-js/07.functions/07-exercises.js
+++ 01.module-js/07.functions/07-exercises.js
@@ -1,15 +1,33 @@
 // escreva uma função que retorna uma string
+const minhaFuncao = () => 'Thiago';
 
 // escreva uma função que retorna a soma de dois numeros
+const soma = () => 1 + 1;
 
 // escreva uma função que recebe dois numeros como parametro e retorna a soma desses valores
+const soma = (num1, num2) => num1 + num2;
 
 // escreva uma função que recebe um numero do tipo string como parametro e soma + 1
+const soma = (string1) => parseInt(string1) + 2;
 
 // escreva uma função que recebe uma string como parametro e retorna um array com todas as letras separadas
+const returnArray = (string1) => string1.split('');
 
 // escreva uma função que recebe uma frase e retorna ela toda em maisculo
+const uppercase = (string1) => string1.toUpperCase();
 
 // escreva uma função que recebe um numero como parametro e retorna outra função que recebe um numero como parametro. O retorno da segunda função deve ser a soma dos dois
+const sum1 = (num1) => (num2) => num1 + num2;
 
 // escreva uma função que recebe uma frase (string) e retorna "Futebol encontrado" se foi encontrado a palavra "futebol" dentro dela ou "Futebol não encontrado"
+const hasFutebol = (string1) => {
+  const sentence = string1.toUpperCase();
+  
+  if (sentence.includes('FUTEBOL')) {
+    console.log('Futebol encontrado!');
+
+    return;
+  }
+
+  console.log('Futebol não encontrado!');
+}
